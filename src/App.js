57af4c2c54a9a15c049f08c6470de9de7926a24f
Melih Karakaya <46146749+Thynic@users.noreply.github.com>
2023-03-27 01:03:31 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import "./App.css";
-import SignInSide from "./Components/Sign/SignInSide";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import SignInSide from './Components/Sign/SignInSide';
 
 function App() {
-  return (
-    <div className="App">
-      <SignInSide />
-    </div>
-  );
+	return (
+		<Router>
+			<div>
+				{/* <nav>
+					<ul>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+					</ul>
+				</nav> */}
+
+				<Switch>
+					<Route path="/login">
+						<SignInSide />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
